c5a7399b949c961b6e3184273d7885dad3d7ab95
Romitan Flaviu <flaviu.romitan23@gmail.com>
2025-05-22 01:32:17 +0300

    completed the communication to the backend trough axios, allowed CORS origins in the backend, made basic routing for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
-    <div className="body">
-      <LoginPage></LoginPage>
-    </div>
+    <BrowserRouter>
+      {/* <div className="body">
+        <LoginPage></LoginPage>
+      </div> */}
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<LoginPage />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,40 @@
 import { useState } from "react";
 import "../styles/LoginPage.css";
+import axios from "axios";
 
 function LoginPage() {
   const [register, setRegister] = useState(false);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [password, setPassword] = useState("");
 
   console.log(register);
+
+  const sendToRegister = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/users/register",
+        { email, username, telephone, password }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendToLogin = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/users/login",
+        {username, password }
+      );
+      console.log(response.data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return register ? (
     <div className="login-body">
@@ -15,16 +45,43 @@ function LoginPage() {
           type="text"
           placeholder="Email"
           className="register-input"
+          value={email}
+          onChange={(val) => setEmail(val.target.value)}
         />
 
-        <input type="text" placeholder="Username" className="register-input" />
+        <input
+          type="text"
+          placeholder="Username"
+          className="register-input"
+          value={username}
+          onChange={(val) => setUsername(val.target.value)}
+        />
 
-        <input type="text" placeholder="Telephone" className="register-input" />
+        <input
+          type="text"
+          placeholder="Telephone"
+          className="register-input"
+          value={telephone}
+          onChange={(val) => setTelephone(val.target.value)}
+        />
 
-        <input type="text" placeholder="Password" className="register-input" />
+        <input
+          type="text"
+          placeholder="Password"
+          className="register-input"
+          value={password}
+          onChange={(val) => setPassword(val.target.value)}
+        />
 
         <div>
-          <button className="button">Continue</button>
+          <button
+            className="button"
+            onClick={() => {
+              sendToRegister();
+            }}
+          >
+            Continue
+          </button>
           <button className="button" onClick={() => setRegister(false)}>
             Go Back
           </button>
@@ -40,12 +97,27 @@ function LoginPage() {
           type="text"
           placeholder="Username or email"
           className="user-input"
+          value={username}
+          onChange={(val) => setUsername(val.target.value)}
         />
 
-        <input type="text" placeholder="Password" className="password-input" />
+        <input
+          type="text"
+          placeholder="Password"
+          className="password-input"
+          value={password}
+          onChange={(val) => setPassword(val.target.value)}
+        />
 
         <div>
-          <button className="button">Login</button>
+          <button
+            className="button"
+            onClick={() => {
+              sendToLogin();
+            }}
+          >
+            Login
+          </button>
           <button className="button" onClick={() => setRegister(true)}>
             Register
           </button>
