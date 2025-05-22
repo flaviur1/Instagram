57dbaa771ae8ab202162d10adcce96fcef95d98d
Romitan Flaviu <flaviu.romitan23@gmail.com>
2025-05-22 17:33:53 +0300

    created a basic home page template

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,29 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/HomePage.css";
 import axios from "../axiosConfig";
 import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
 
-  console.log(localStorage.getItem("token"));
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await axios.get("/api/posts/getAllPosts");
+        setPosts(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  const getData = async () => {
-    try {
-      const response = await axios.get("/api/posts/getAll", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    getPosts();
+  }, []);
 
-  getData();
-
-  return <div></div>;
+  return (
+    <div className="posts-container">
+      {posts.map((post) => (
+        <div className="post-card" key={post.id}>
+          <h3>{post.title || "Untitled Post"}</h3>
+          <h4>{post.userId.username || "null"}</h4>
+          <p>{post.content || JSON.stringify(post)}</p>
+          {/* Add more fields as needed */}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
